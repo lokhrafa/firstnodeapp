e4863a5018fb6243251780ed9e930809e0cf6731
Rafael Batista Arroyo <lokhrafael@hotmail.com>
2019-03-26 13:09:33 -0400

    Add files via upload

---
+++ firstnodeapp/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+
+app.use(require('./routes'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
